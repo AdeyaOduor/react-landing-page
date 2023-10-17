405e25ebefb524f51632228cc4abf213311b7c27
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-10-17 22:37:42 +0300

    Create chart0.js

---
+++ src/chart0.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  chartContainer: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const fetchChartDataFromAPI = () => {
+  return axios.get('https://api.example.com/chart-data');
+};
+
+const ChartComponent = () => {
+  const classes = useStyles();
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchChartDataFromAPI();
+        const data = response.data;
+        setChartData(data);
+      } catch (error) {
+        console.error('Error fetching chart data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={classes.chartContainer}>
+      {chartData ? (
+        <>
+          <Typography variant="h6">Sales Chart</Typography>
+          <Bar data={chartData} />
+        </>
+      ) : (
+        <Typography variant="body1">Loading chart data...</Typography>
+      )}
+    </div>
+  );
+};
+
+export default ChartComponent;
